1e9121cd6aaa06428b75432f1e2e4bb7d5c26f14
sanku <san.kukkonen@gmail.com>
2021-12-07 15:59:59 +0200

    bäackendia eteenpäin mut ei tod valmis vielä eikä edes toimi

--- server/routes/api/projects.js
+++ server/routes/api/projects.js
@@ -1,16 +1,57 @@
 
 const express = require('express');
 const mongodb = require('mongodb');
-
 const router = express.Router();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploadedImg/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+const fileFilter = (req, file, cb) => { // tekiskö try/catch?
+  if(file.mimetype === 'image/jpeg'
+      || file.mimetype === 'image/png'
+      || file.mimetype === 'image/gif'){
+    // accept file
+    cb(null, true);
+  } else {
+    // reject file
+    cb(new Error(Error.message), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
 
 // get posts
-router.get('/', async (req, res) => {
-
+router.get('/', upload.single('projectImage'), async (req, res) => {
+  console.log(req.file);
   const projects = await loadProjectCollection();
   res.send(await projects.find({}).toArray()); // find all
 });
 
+/*
+// leevi's add post
+router.post('/api/db', upload.fields([{
+  name: 'file',
+}, {
+  name: 'files',
+}]), function(req, res){
+  const formData = req.body;
+  console.log('form data', formData);
+  console.log('img', formData.title);
+  res.send(req.file);
+});*/
 
 // add posts
 router.post('/', async (req, res) => {
